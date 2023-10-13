1f7be0304819374b7bd530ff62139087897dc456
daquioag <aldaquioag@gmail.com>
2023-10-12 22:37:37 -0700

    implement client side scripts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require("url");
 
+
 const mysql = require("mysql");
 const db = mysql.createConnection({
     host: "localhost",
@@ -9,11 +10,11 @@ const db = mysql.createConnection({
     database: "compcom_nodemysql"
 });
 
-con.connect(function(err) {
+db.connect(function(err) {
     if (err) throw err;
     console.log("Connecting to database....");
-    const sql = "CREATE TABLE IF NOT EXISTS patients (patientId INT(11), name VARCHAR(100), dateOfBirth DATETIME)"; 
-    con.query(sql, function (err, result) {
+    const sql = "CREATE TABLE IF NOT EXISTS patients (patientId INT(11)  NOT NULL AUTO_INCREMENT, name VARCHAR(100), dateOfBirth DATETIME, PRIMARY KEY (Personid))"; 
+    db.query(sql, function (err, result) {
       if (err) throw err;
       console.log("CONNECTED! Table exsits or was created!");
     });
@@ -34,11 +35,12 @@ const server = http.createServer(function (req, res) {
         res.end();
         return;
     }
-    const parsedUrl = url.parse(req.url, true);
-    const new_query = parsedUrl.query.query;
+    let new_query;
+
+
     if (req.method === "GET"){
         const parsedUrl = url.parse(req.url, true);
-        const new_query = parsedUrl.query.query;
+        new_query = parsedUrl.query.query;
 
         console.log("Executing SQL query:", new_query);
 
@@ -53,6 +55,40 @@ const server = http.createServer(function (req, res) {
         });
     }
 
+    if (req.method === "POST" && new_query){
+
+        console.log("Executing SQL query:", new_query.trim());
+
+        db.query(new_query, function(err, result) {
+            if (err) throw err;
+            console.log("Query result:", result);
+            // Assuming the result is an array of rows
+            const response = JSON.stringify(result);
+
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(response);
+        });
+    } else{
+        const insertSql = "INSERT INTO patients (name, dateOfBirth) VALUES (?, ?)";
+        const values = [
+          ['Sara Brown', '1901-01-01'],
+          ['John Smith', '1941-01-01'],
+          ['Jack Ma', '1911-01-30'],
+          ['Elon Musk', '1999-01-01']
+        ];
+        
+
+        const sql = "INSERT INTO patients name dateOfBirth values ('Sara Brown', 1901-01-01), ('Tanner Brown', 1901-01-01),  "; 
+        db.query(insertSql, values, function(err, result) {
+            if (err) throw err;
+            console.log("Query result:", result);
+            // Assuming the result is an array of rows
+            const response = JSON.stringify(result);
+
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(response);
+        });   
+    }
 
 });
 server.listen();
