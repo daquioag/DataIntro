c799fb4cd7f1b4455f4f909f037f17c9ac3d9f41
Thant Yati Thaw <tthaw@my.bcit.ca>
2023-10-18 23:43:08 -0700

    Stored remaining hard-coded strings from console.log at the top

--- app.js
+++ app.js
@@ -1,43 +1,54 @@
 const http = require("http");
 const url = require("url");
-
 const mysql = require("mysql");
+
+// Define all hard-coded strings and constants
+const STRINGS = {
+    DATABASE_ERROR: "Error connecting to the database:",
+    DATABASE_CONNECTED: "Connected to the database",
+    TABLE_CREATION_ERROR: "Error creating table:",
+    TABLE_EXISTS_OR_CREATED: "CONNECTED! Table exists or was created!",
+    EXECUTING_SQL: "Executing SQL query:",
+    SQL_ERROR: "Error executing SQL query:"
+};
 
 const ERROR_BAD_REQUEST = {
     error: "Bad Request",
     details: "Query parameter is missing or empty"
 };
+
 const ERROR_METHOD_NOT_ALLOWED = {
     error: "Method Not Allowed"
 };
-const ERROR_INTERNAL_SERVER = "Internal Server Error";
+
+const ERROR_INTERNAL_SERVER = STRINGS.SQL_ERROR;
 const SQL_PATIENTS_TABLE = "CREATE TABLE IF NOT EXISTS patients (patientId INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(100), dateOfBirth DATETIME, PRIMARY KEY (patientId))";
 const MESSAGE_SQL_PROCESSED = "SQL Query processed!";
 
 const db = mysql.createConnection({
     host: "localhost",
     user: "compcom_compcom",
-    password: "*******",
+    password: "*******", 
     database: "compcom_nodemysql",
 });
 
 db.connect(function (err) {
     if (err) {
-        console.error("Error connecting to the database:", err);
+        console.error(STRINGS.DATABASE_ERROR, err);
         return;
     }
-    console.log("Connected to the database");
+    console.log(STRINGS.DATABASE_CONNECTED);
+    
     db.query(SQL_PATIENTS_TABLE, function (err, result) {
         if (err) {
-            console.error("Error creating table:", err);
+            console.error(STRINGS.TABLE_CREATION_ERROR, err);
             // Handle the error appropriately, e.g., return an error response to the client
             return;
         }
-        console.log("CONNECTED! Table exsits or was created!");
+        console.log(STRINGS.TABLE_EXISTS_OR_CREATED);
         console.log("Query result:", result);
     });
 });
-
 
 const server = http.createServer(function (req, res) {
     const parsedUrl = url.parse(req.url);
@@ -65,8 +76,8 @@ const server = http.createServer(function (req, res) {
             res.end(JSON.stringify(ERROR_BAD_REQUEST));
             return;
         }
-        console.log("Executing SQL query:", new_query);
 
+        console.log(STRINGS.EXECUTING_SQL, new_query);
         db.query(new_query, function (err, result) {
             handleQueryError(res, err);
             console.log("Query result:", result);
@@ -107,13 +118,12 @@ const server = http.createServer(function (req, res) {
 
     function handleQueryError(res, err) {
         if (err) {
-            console.error("Error executing SQL query:", err);
+            console.error(STRINGS.SQL_ERROR, err);
             res.writeHead(500, { "Content-Type": "application/json" });
-            res.end(
-                JSON.stringify({ error: ERROR_INTERNAL_SERVER, details: err.message })
-            );
+            res.end(JSON.stringify({ error: ERROR_INTERNAL_SERVER, details: err.message }));
             return;
         }
     }
 });
+
 server.listen();
